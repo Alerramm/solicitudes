b9b1a0a21982a684fc7c5292904dbd37078bb167
Alejandro Galicia <out_agaliciag@independencia.com.mx>
2020-08-18 15:30:37 -0500

    process master

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,5 +1,5 @@
-const BASE_CONFIRMATION_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/';
-const BASE_OPERATIONS_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/solicitudes/QA/';
+const BASE_CONFIRMATION_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/';
+const BASE_OPERATIONS_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/solicitudes/';
 
 /** CONSULTA CLIENTES */
 export const CONSULTAR_VIAJES = BASE_OPERATIONS_URL + 'get/viajes.php';
